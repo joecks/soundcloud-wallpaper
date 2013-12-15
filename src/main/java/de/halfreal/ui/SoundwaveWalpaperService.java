5b13c2adf91393ac9e72aa8c45e884757adc9207
joecks <joecks@halfreal.de>
2013-12-15 21:37:14 +0100

    test functionality on Android fixed Manifest declaration added wallpaper xml definition

--- src/main/java/de/halfreal/ui/SoundwaveWalpaperService.java
+++ src/main/java/de/halfreal/ui/SoundwaveWalpaperService.java
@@ -1,22 +1,36 @@
 package de.halfreal.ui;
 
+import android.provider.ContactsContract.CommonDataKinds.Photo;
 import android.service.wallpaper.WallpaperService;
+import de.halfreal.net.SoundwaveServiceModule.SoundwaveModuleListener;
 
-public class SoundwaveWalpaperService extends WallpaperService {
+public class SoundwaveWalpaperService extends WallpaperService implements
+		SoundwaveModuleListener {
 
 	private class SoundwaveEngine extends Engine {
+
+	}
+
+	public void newPhotoReceived(Photo photo) {
 
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
+		SoundwaveApplication.soundwaveModule.registerListener(this);
+		SoundwaveApplication.soundwaveModule.downloadNewWaveform();
 	}
 
 	@Override
 	public Engine onCreateEngine() {
 		return new SoundwaveEngine();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		SoundwaveApplication.soundwaveModule.unregisterListener(this);
 	}
 
 }
