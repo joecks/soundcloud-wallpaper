fccf0e0b940f41f00c69fd65d33ea953d2c77b59
joecks <joecks@halfreal.de>
2013-12-17 01:21:17 +0100

    added module tests

--- src/test/java/de/halfreal/test/TracksServiceTest.java
+++ src/test/java/de/halfreal/test/TracksServiceTest.java
@@ -2,21 +2,30 @@ package de.halfreal.test;
 
 import static junit.framework.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import retrofit.Callback;
 import retrofit.RestAdapter;
-import android.service.wallpaper.WallpaperService.Engine;
 import de.halfreal.model.Track;
 import de.halfreal.net.API;
+import de.halfreal.net.SoundwaveServiceModule;
+import de.halfreal.net.SoundwaveServiceModule.SoundwaveModuleListener;
 import de.halfreal.net.TrackService;
 import de.halfreal.test.net.TrackServiceSync;
 import de.halfreal.test.robolectric.RobolectricMavenTestRunner;
 import de.halfreal.ui.SoundwaveApplication;
-import de.halfreal.ui.SoundwaveWallpaperService;
 
 @RunWith(RobolectricMavenTestRunner.class)
 public class TracksServiceTest {
+
+	protected Boolean called;
+
+	@Before
+	public void beforeTest() {
+		called = null;
+	}
 
 	public TrackServiceSync createMockTrackService() {
 
@@ -39,19 +48,7 @@ public class TracksServiceTest {
 	}
 
 	@Test
-	public void downloadImage() {
-		SoundwaveApplication soundwaveApplication = new SoundwaveApplication();
-		soundwaveApplication.onCreate();
-		SoundwaveApplication.trackService = createMockTrackServiceAsync();
-
-		SoundwaveWallpaperService soundwaveWalpaperService = new SoundwaveWallpaperService();
-		soundwaveWalpaperService.onCreate();
-		Engine onCreateEngine = soundwaveWalpaperService.onCreateEngine();
-
-	}
-
-	@Test
-	public void queryForTracksFromMockServiceTest() throws InterruptedException {
+	public void testJSONTOModelDeserialization() throws InterruptedException {
 		TrackServiceSync createMockTrackService = createMockTrackService();
 		Track[] tracks = createMockTrackService.tracks(API.CLIENT_ID,
 				"zoekeating");
@@ -62,6 +59,47 @@ public class TracksServiceTest {
 		assertNotNull(tracks[0].getWaveform_url());
 		assertEquals("http://w1.sndcdn.com/MEkayzDwTueX_m.png",
 				tracks[0].getWaveform_url());
+	}
+
+	@Test
+	public void testModuleListeners() {
+
+		SoundwaveApplication.trackService = new TrackService() {
+
+			public void tracks(String clientId, String user,
+					Callback<Track[]> response) {
+				response.success(null, null);
+			}
+		};
+		SoundwaveServiceModule soundwaveServiceModule = new SoundwaveServiceModule();
+		SoundwaveModuleListener listener = new SoundwaveModuleListener() {
+
+			public void newTrackReceived(Track track) {
+				called = true;
+			}
+		};
+
+		soundwaveServiceModule.registerListener(listener);
+		soundwaveServiceModule.downloadNewWaveform();
+
+		assertNull(called);
+
+		SoundwaveApplication.trackService = new TrackService() {
+
+			public void tracks(String clientId, String user,
+					Callback<Track[]> response) {
+				Track[] tracks = new Track[] { new Track() };
+				response.success(tracks, null);
+			}
+		};
+		soundwaveServiceModule.downloadNewWaveform();
+
+		assertTrue(called);
+		called = null;
+
+		soundwaveServiceModule.unregisterListener(listener);
+		soundwaveServiceModule.downloadNewWaveform();
+		assertNull(called);
 	}
 
 }
